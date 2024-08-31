d1417a7de2435e3074a847288633341f73b53e17
NoodleSushi <ytmikesstuffs@gmail.com>
2024-09-01 02:54:25 +0800

    chore: Bump up setup.py version to 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Filipino Native Language Identifier',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     include_package_data=True,
     url='',
