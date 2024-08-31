db417cd128c426796373021d71f57a04e3c9fca5
NoodleSushi <ytmikesstuffs@gmail.com>
2024-09-01 02:37:51 +0800

    Refactor LanguageModels.py to use importlib.resources for file loading

--- phidentifier/LanguageModels.py
+++ phidentifier/LanguageModels.py
@@ -3,9 +3,9 @@ from encodings.aliases import aliases
 from . import Markov
 import importlib.resources
 
-model_cebuano = Markov.MarkovChain(importlib.resources.open_text('phidentifier', '/data/processed/cebuano.txt', encoding='utf8'), 'Cebuano')
-model_kapampangan = Markov.MarkovChain(importlib.resources.open_text('phidentifier', '/data/processed/kapampangan.txt', encoding='utf8'),'Kapampangan')
-model_pangasinan = Markov.MarkovChain(importlib.resources.open_text('phidentifier', '/data/processed/pangasinense.txt', encoding='utf8'), 'Pangasinan')
+model_cebuano = Markov.MarkovChain(importlib.resources.open_text('phidentifier.data.processed', 'cebuano.txt', encoding='utf8'), 'Cebuano')
+model_kapampangan = Markov.MarkovChain(importlib.resources.open_text('phidentifier.data.processed', 'kapampangan.txt', encoding='utf8'),'Kapampangan')
+model_pangasinan = Markov.MarkovChain(importlib.resources.open_text('phidentifier.data.processed', 'pangasinense.txt', encoding='utf8'), 'Pangasinan')
 
 
 
